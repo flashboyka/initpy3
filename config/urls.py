249e2a2b1b921d442618cd8e63010f628c758050
Pavel Tanchev <dcopm999@gmail.com>
2019-07-09 18:34:10 +0500

    urls.py добавил урл для переключения языка

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
-from django.contrib import admin
+
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
@@ -16,6 +18,7 @@ urlpatterns = [
     path("users/", include("initpy.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path('i18n/', include('django.conf.urls.i18n')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
