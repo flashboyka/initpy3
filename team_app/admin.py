abfa9ea6d0787ba5429cdaa8035981e7e0f36085
Pavel Tanchev <dcopm999@gmail.com>
2019-07-10 11:46:22 +0500

    team_app.admin
    Добавил в админку приложение team_app

---
+++ team_app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from team_app.models import TeamModel
+from team_app.forms import TeamForm
+# Register your models here.
+
+
+@admin.register(TeamModel)
+class TeamAdmin(admin.ModelAdmin):
+    form = TeamForm
+    list_display = ['user', 'skills']
+    autocomplete_fields = ('user',)
