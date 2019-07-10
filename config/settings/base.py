37b85f554911584e2c0f6b0e8f873003cfdb0413
Pavel Tanchev <dcopm999@gmail.com>
2019-07-10 19:10:17 +0500

    settings.py
    Добавил настройки пакета django-suit

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,6 +3,7 @@ Base settings to build other settings files upon.
 """
 import os
 import environ
+from django.utils.translation import ugettext_lazy as _
 
 ROOT_DIR = (
     environ.Path(__file__) - 3
@@ -369,3 +370,40 @@ CKEDITOR_CONFIGS = {
 }
 
 # ------------------------------------------------------------------------------
+# django-suit
+# ------------------------------------------------------------------------------
+SUIT_CONFIG = {
+    # header
+    'ADMIN_NAME': 'init.py',
+    # 'HEADER_DATE_FORMAT': 'l, j. F Y',
+    # 'HEADER_TIME_FORMAT': 'H:i',
+
+    # forms
+    # 'SHOW_REQUIRED_ASTERISK': True,  # Default True
+    # 'CONFIRM_UNSAVED_CHANGES': True, # Default True
+
+    # menu
+    # 'SEARCH_URL': '/admin/auth/user/',
+    # 'MENU_ICONS': {
+    #    'sites': 'icon-leaf',
+    #    'auth': 'icon-lock',
+    # },
+    # 'MENU_OPEN_FIRST_CHILD': True, # Default True
+    # 'MENU_EXCLUDE': ('auth.group',),
+    'MENU': (
+        'sites',
+        {'label': _('Accounts'),
+         'icon': 'icon-user',
+         'models': (
+             'team_app.teammodel',
+             'users.user',
+             'auth.group'
+         )}
+        # {'app': 'auth', 'icon':'icon-lock', 'models': ('user', 'group')},
+        # {'label': 'Settings', 'icon':'icon-cog', 'models': ('auth.user', 'auth.group')},
+        # {'label': 'Support', 'icon':'icon-question-sign', 'url': '/support/'},
+    ),
+
+    # misc
+    # 'LIST_PER_PAGE': 15
+}
