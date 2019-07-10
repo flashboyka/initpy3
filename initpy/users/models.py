042cf8f1a1ee1ebcf5d92c104ea2a2abcbd62231
Pavel Tanchev <dcopm999@gmail.com>
2019-07-10 11:51:00 +0500

    initpy.models: Добавил поля photo и birth_day

--- initpy/users/models.py
+++ initpy/users/models.py
@@ -1,14 +1,19 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
+from sorl.thumbnail import ImageField
+
 
 class User(AbstractUser):
-
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    photo = ImageField(upload_to="team", blank=True, verbose_name=_("Photo"))
+    birth_day = models.DateField(null=True, blank=True, verbose_name=_("Birth day"))
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def __str__(self):
+        return "{0} {1} ({2})".format(self.first_name, self.last_name, self.username)
