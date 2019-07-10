3e19f010d149a08f112eda57fd38ebb25f054d37
Pavel Tanchev <dcopm999@gmail.com>
2019-07-10 11:51:54 +0500

    initpy.users.admin: Добавил отображение новых полей в админке

--- initpy/users/admin.py
+++ initpy/users/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from django.utils.translation import ugettext_lazy as _
+
+from sorl.thumbnail.admin import AdminImageMixin
 
 from initpy.users.forms import UserChangeForm, UserCreationForm
 
@@ -8,10 +11,14 @@ User = get_user_model()
 
 
 @admin.register(User)
-class UserAdmin(auth_admin.UserAdmin):
-
+class UserAdmin(AdminImageMixin, auth_admin.UserAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
-    list_display = ["username", "name", "is_superuser"]
+    list_display = ["username", "is_superuser", "birth_day"]
     search_fields = ["name"]
+    fieldsets = auth_admin.UserAdmin.fieldsets+(
+        (_("Advanced options"), {
+            "classes": ('collapse'),
+            "fields": ("birth_day", "photo",)
+        }),
+    )
